3428e7578db59964fb26f93b880e15afb42eab5f
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-12-18 20:27:39 +0700

    Membuat ContainerApp

---
+++ app/src/main/java/com/example/questapi_088/repositori/ContainerApp.kt
@@ -0,0 +1,54 @@
+package com.example.questapi_088.repositori
+
+import android.app.Application
+import com.example.questapi_088.apiservice.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+interface ContainerApp {
+    val repositoriDataSiswa : RepositoriDataSiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    private val baseurl = "http://10.0.2.2/pertemuan12/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseurl)
+        .addConverterFactory(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }.asConverterFactory("application/json".toMediaType())
+        )
+        .client(klien)
+        .build()
+
+    private val retrofitService : ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+
+    override val repositoriDataSiswa: RepositoriDataSiswa by lazy {
+        JaringanRepositoriDataSiswa(retrofitService)
+    }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container : ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
