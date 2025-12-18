7e4eb6fcc85b05df13ca85aaa351e6789f46ea31
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-12-18 20:43:50 +0700

    Memperbaiki HomeViewModel.kt

--- app/src/main/java/com/example/questapi_088/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/questapi_088/viewmodel/HomeViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.questapi_088.viewmodel
 
-import android.net.http.HttpException
+
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.questapi_088.modeldata.DataSiswa
 import com.example.questapi_088.repositori.RepositoriDataSiswa
 import kotlinx.coroutines.launch
 import java.io.IOException
+import retrofit2.HttpException
+
 
 sealed interface StatusUiSiswa {
     data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
