25afa880270de1f04fd6e5189dda97522248ffc5
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-12-25 23:27:40 +0700

    memperbaiki error di PetaNavigasi.kt

--- app/src/main/java/com/example/questapi_088/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_088/uicontroller/PetaNavigasi.kt
@@ -3,43 +3,60 @@ package com.example.questapi_088.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.questapi_088.uicontroller.route.DestinasiDetail
+import com.example.questapi_088.uicontroller.route.DestinasiEdit
 import com.example.questapi_088.uicontroller.route.DestinasiEntry
 import com.example.questapi_088.uicontroller.route.DestinasiHome
+import com.example.questapi_088.view.DetailSiswaScreen
+import com.example.questapi_088.view.EditSiswaScreen
 import com.example.questapi_088.view.EntrySiswaScreen
 import com.example.questapi_088.view.HomeScreen
 
 
 @Composable
-fun DataSiswaApp(navController: NavHostController = rememberNavController(),
-                 modifier: Modifier){
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
     HostNavigasi(navController = navController)
 }
 
 @Composable
 fun HostNavigasi(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
+    navController: NavHostController,modifier: Modifier = Modifier
 ){
-    NavHost(navController = navController, startDestination = DestinasiHome.route,
-        modifier = Modifier){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier ) {
         composable(DestinasiHome.route) {
             HomeScreen(
-                navigateToItemEntry = {
-                    navController.navigate(DestinasiEntry.route)
-                },
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 navigateToItemUpdate = {
                     navController.navigate("${DestinasiDetail.route}/${it}")
                 },
                 modifier = modifier)
         }
-        composable(DestinasiEntry.route){
-            EntrySiswaScreen(navigateBack = {
-                navController.navigate(DestinasiHome.route)
-            })
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+        composable(
+            DestinasiDetail.routeWithArgs, arguments = listOf(navArgument(DestinasiDetail
+                .itemIdArg) {
+                type = NavType.IntType })
+        ){
+            // --- PERBAIKAN DI BARIS INI ---
+            DetailSiswaScreen(
+                navigateToEditItem = { navController.navigate("${DestinasiEdit.route}/${it}") }, // Spasi dihilangkan
+                navigateBack = { navController.navigate(DestinasiHome.route) }
+            )
+            // --- AKHIR PERBAIKAN ---
+        }
+        composable(
+            DestinasiEdit.routeWithArgs, arguments = listOf(navArgument(DestinasiEdit.itemIdArg
+            ){
+                type = NavType.IntType})){
+            EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() })
         }
     }
 }
--- app/src/main/java/com/example/questapi_088/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_088/viewmodel/provider/PenyediaViewModel.kt
@@ -1,26 +1,34 @@
 package com.example.questapi_088.viewmodel.provider
 
-import android.app.Application
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi_088.repositori.AplikasiDataSiswa
+import com.example.questapi_088.viewmodel.DetailViewModel
+import com.example.questapi_088.viewmodel.EditViewModel
 import com.example.questapi_088.viewmodel.EntryViewModel
 import com.example.questapi_088.viewmodel.HomeViewModel
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa {
-    val app = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as Application
-    return app as AplikasiDataSiswa
-}
-
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa) }
         initializer {
-            HomeViewModel(aplikasiDataSiswa().container.repositoriDataSiswa)
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().containerApp.repositoryDataSiswa
+            )
         }
         initializer {
-            EntryViewModel(aplikasiDataSiswa().container.repositoriDataSiswa)
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().containerApp.repositoryDataSiswa
+            )
         }
     }
 }
