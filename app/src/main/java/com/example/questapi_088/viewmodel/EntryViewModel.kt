d5fb5816e848ccd0ad98e8c7bd5e7fb37bbcee4a
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-12-25 23:28:38 +0700

    membuat file EditViewModel

--- app/src/main/java/com/example/questapi_088/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questapi_088/viewmodel/EntryViewModel.kt
@@ -4,35 +4,38 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.questapi_088.modeldata.DataSiswa
 import com.example.questapi_088.modeldata.DetailSiswa
 import com.example.questapi_088.modeldata.UIStateSiswa
 import com.example.questapi_088.modeldata.toDataSiswa
-import com.example.questapi_088.repositori.RepositoriDataSiswa
-
+import com.example.questapi_088.repositori.RepositoryDataSiswa
 import retrofit2.Response
 
-class EntryViewModel (private val repositoryDataSiswa: RepositoriDataSiswa) : ViewModel(){
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) :
+    ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
-    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
-        return with(uiState){
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa):
+            Boolean {
+        return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
 
-    fun updateUiState(detailSiswa: DetailSiswa){
-        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
-
-    suspend fun addSiswa(){
-        if (validasiInput()){
+    suspend fun addSiswa() {
+        if (validasiInput()) {
             val sip: Response<Void> = repositoryDataSiswa
                 .postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
             if (sip.isSuccessful){
-                println("Sukses Tambah Data : ${sip.message()}")
+                println("Sukses tambah data : ${sip.message()}")
             }else {
-                println("Gagal Tambah Data : ${sip.message()}")
+                println("Gagal tambah data : ${sip.errorBody()}")
             }
         }
     }
