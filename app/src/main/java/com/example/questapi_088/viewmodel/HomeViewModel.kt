a5e4a0a5af5e14a83b9d2c56a0ab126b42ea543e
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-12-25 23:31:19 +0700

    memperbaiki file HomeViewModel.kt

--- app/src/main/java/com/example/questapi_088/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/questapi_088/viewmodel/HomeViewModel.kt
@@ -1,26 +1,24 @@
 package com.example.questapi_088.viewmodel
 
-
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.questapi_088.modeldata.DataSiswa
-import com.example.questapi_088.repositori.RepositoriDataSiswa
+import com.example.questapi_088.repositori.RepositoryDataSiswa
 import kotlinx.coroutines.launch
-import java.io.IOException
 import retrofit2.HttpException
-
+import java.io.IOException
 
 sealed interface StatusUiSiswa {
-    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    data class Success(val Siswa: List<DataSiswa>) : StatusUiSiswa
     object Error : StatusUiSiswa
     object Loading : StatusUiSiswa
 }
 
-class HomeViewModel (private val repositoryDataSiswa: RepositoriDataSiswa):
-    ViewModel() {
+
+class HomeViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
     var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
         private set
 
@@ -32,12 +30,10 @@ class HomeViewModel (private val repositoryDataSiswa: RepositoriDataSiswa):
         viewModelScope.launch {
             listSiswa = StatusUiSiswa.Loading
             listSiswa = try {
-                StatusUiSiswa.Success(repositoryDataSiswa
-                    .getDataSiswa())
-            }catch (e: IOException){
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            } catch (e: IOException) {
                 StatusUiSiswa.Error
-            }
-            catch (e: HttpException){
+            } catch (e: HttpException) {
                 StatusUiSiswa.Error
             }
         }
